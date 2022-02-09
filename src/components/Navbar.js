7f24dfd6d2e6d78dca773d4ce3ce35620b24144d
Aditya <adityadond281@gmail.com>
2022-02-09 21:42:53 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class NavBar extends Component {
+  render() {
+    return <div>
+        <div className="col-12">
+            <h2 className="bg-primary text-white text-center p2">
+              {this.props.name}`s ToDo list
+            </h2>
+          </div>
+    </div>;
+  }
+}
+
+export default NavBar;
